976796adcae0a258ec91ed30725bda4c56ba7d10
erikamaylim <erikamayerlima@gmail.com>
2023-03-24 16:07:04 -0300

    Curso Java Básico da Loiane

--- src/com/ka/cursojava/aula13/labs/Ex008.java
+++ src/com/ka/cursojava/aula13/labs/Ex008.java
@@ -8,10 +8,13 @@ import java.util.Scanner;
 public class Ex008 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Digite quanto você ganha por hora trabalhada: ");
         double valorHora = scan.nextDouble();
+
         System.out.print("Digite o número de horas trabalhadas no mês: ");
         double horasTrabalhadas = scan.nextDouble();
+
         double salario = valorHora * horasTrabalhadas;
         System.out.printf("Este mês, você receberá R$ %.2f %n", salario);
     }
