af4f038bf9e1ebdaf1c22f7c699abeeb6e7c2062
erikamaylim <erikamayerlima@gmail.com>
2023-03-24 15:33:13 -0300

    Curso Java Básico da Loiane

--- src/com/ka/cursojava/aula13/labs/Ex001.java
+++ src/com/ka/cursojava/aula13/labs/Ex001.java
@@ -1,4 +1,10 @@
+// Faça um programa que mostre a mensagem "Alo mundo" na tela.
+
 package com.ka.cursojava.aula13.labs;
 
 public class Ex001 {
+    public static void main(String[] args) {
+        System.out.println("Alo mundo");
+    }
+
 }
