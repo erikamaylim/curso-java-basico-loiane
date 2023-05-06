7ebff60ba8ec9836068fe0e0b18b6c5e64b04cf2
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-05-06 16:17:10 -0300

    Novos

--- src/com/ka/cursojava/labs/Ex102.java
+++ src/com/ka/cursojava/labs/Ex102.java
@@ -4,8 +4,28 @@ vetor A.
 
 package com.ka.cursojava.labs;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class Ex102 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] vetorA = new int[5];
+
+        for(int i = 0; i < vetorA.length; i++) {
+            System.out.printf("%dº valor do vetor A: ", i + 1);
+            vetorA[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < vetorA.length; i++) {
+            System.out.printf("%n Tabuada do %d: %n", vetorA[i]);
+            for (int j = 1; j <= 10; j++){
+                System.out.printf("%2d x %2d = %3d %n", vetorA[i], j, vetorA[i] * j);
+            }
+
+        }
 
     }
 }
