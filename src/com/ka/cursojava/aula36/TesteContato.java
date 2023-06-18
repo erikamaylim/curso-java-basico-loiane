1047b7d37e899831900cf6bb0d391aa8bb778e5b
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-06-18 14:31:11 -0300

    Update

--- src/com/ka/cursojava/aula36/TesteContato.java
+++ src/com/ka/cursojava/aula36/TesteContato.java
@@ -7,8 +7,12 @@ package com.ka.cursojava.aula36;
 public class TesteContato {
     public static void main(String[] args) {
         Contato c1 = new Contato();
+
+        //relacionamento tem-um endereco
         Endereco e1 = new Endereco("Morro das Sombras", "n/a"
                 , "n/a", "22222-222", "Camorr", "Therin");
+
+        //relacionamento tem-um telefone
         Telefone telefone1 = new Telefone("Celular", "21", "9999-9999");
         Telefone telefone2 = new Telefone("Residencial", "21", "2555-5555");
 
@@ -20,7 +24,9 @@ public class TesteContato {
         c1.setEndereco(e1);
         c1.setTelefones(t1);
 
+        //Teste saída no console
         System.out.println(c1.getNome());
+
         if (c1 != null && c1.getEndereco() != null) {   // Este if é para evitar que ocorra um NullPointerException
             System.out.println(c1.getEndereco().getCidade());
         }
