72abeec0067b8673418bb66ef617712aea36ae14
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-06-20 11:44:31 -0300

    Add aula37

---
+++ src/com/ka/cursojava/aula37/aula37.java
@@ -0,0 +1,12 @@
+package com.ka.cursojava.aula37;
+
+public class aula37 {
+    public static void main(String[] args) {
+//        Aluno a1 = new Aluno();
+//        a1.setNome("Erika");
+//        System.out.println(a1.getNome());
+//        a1.setTelefoneCelular("9999-9999");
+
+        Pessoa aluno = new Aluno();  // Não dá acesso aos métodos da classe Aluno
+    }
+}
