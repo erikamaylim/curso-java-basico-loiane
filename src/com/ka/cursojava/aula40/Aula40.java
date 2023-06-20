2d358aa8a6c0e9830c272487165b175284a21855
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-06-20 14:46:40 -0300

    New

---
+++ src/com/ka/cursojava/aula40/Aula40.java
@@ -0,0 +1,23 @@
+package com.ka.cursojava.aula40;
+
+public class Aula40 {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        Pessoa a1 = new Aluno();
+        Aluno a2 = new Aluno();
+        Pessoa prof1 = new Professor();
+
+        p1.setEndereco("Rua 1, número 1");
+        a1.setEndereco("Rua 2, número 2");
+        prof1.setEndereco("Rua 3, número 3");
+        p1.metodoClasseMae();
+        a1.metodoClasseMae();
+//        a1.metodoClasseFilha();  // Não é possível invocar método da classe Aluno
+        a2.metodoClasseMae();
+        a2.metodoClasseFilha();
+
+        System.out.println(p1.obterEtiquetaEndereco());
+        System.out.println(a1.obterEtiquetaEndereco());
+        System.out.println(prof1.obterEtiquetaEndereco());
+    }
+}
