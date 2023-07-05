92a99e95a1f822ff1ffcb205b0dae9ac4b704c6f
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-07-05 10:15:39 -0300

    new

--- src/com/ka/cursojava/aula47/Aula47.java
+++ src/com/ka/cursojava/aula47/Aula47.java
@@ -5,6 +5,26 @@ package com.ka.cursojava.aula47;
 
 public class Aula47 {
     public static void main(String[] args) {
+        /*int[] vetor = new int[4];
+
+        System.out.println("Antes da exception");
+
+        vetor[4] = 1;
+
+        System.out.println("Esse texto não será impresso");*/   // Esse trecho terá um erro de exceção
+
+        try {
+            int[] vetor = new int[4];
+            System.out.println("Antes da exception");
+
+            vetor[4] = 1;
+
+            System.out.println("Esse texto não será impresso");
+        } catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("Exceção ao acessar um índice que não existe no vetor");
+        }
+
+        System.out.println("Esse texto será impresso após a exception");
 
     }
 }
