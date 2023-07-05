7f170ad9815bb0e223f986c1785ddd4922f0d40d
erikamaylim <94486280+erikamaylim@users.noreply.github.com>
2023-07-05 11:35:00 -0300

    new

--- src/com/ka/cursojava/aula49/FinallyPegadinha.java
+++ src/com/ka/cursojava/aula49/FinallyPegadinha.java
@@ -1,6 +1,6 @@
 package com.ka.cursojava.aula49;
 
-public class Aula49 {
+public class FinallyPegadinha {
     public static void main(String[] args) {
         int[] numeradores = {4, 8, 16, 32, 64, 128};
         int[] denominadores = {2, 0, 4, 8, 0};
@@ -10,10 +10,13 @@ public class Aula49 {
                 System.out.println(numeradores[i] + " / " + denominadores[i] + " = " + numeradores[i] / denominadores[i]);
             } catch (ArithmeticException exception) {
                 System.out.println("Erro ao dividir por zero");
+                System.exit(0);
             } catch (ArrayIndexOutOfBoundsException exception) {
                 System.out.println("Posição do array inválida");
+                System.exit(0);
             } finally {
                 System.out.println("Essa linha é impressa sempre após o try ou o catch");
+                System.out.println();
             }
         }
     }
